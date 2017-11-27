a24fef666447bb2c41e78f17f44b7c584cddcd81
Tom <tjpalmer@tjpalmer.com>
2017-11-27 06:40:44 -0700

    Some ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {PureComponent} from 'react';
 import {render} from 'react-dom';
 
 addEventListener('load', main);
@@ -6,12 +7,13 @@ addEventListener('load', main);
 function main() {
   // let loan = {}
   // let calculator = new LoanCalculator(loan);
-  render(<div>Hello, world!</div>, document.getElementById('root'));
+  let loan: Loan = {months: 0, payment: 0, principal: 0, rate: 0};
+  render(<LoanForm loan={loan}/>, document.getElementById('root'));
 }
 
 interface Loan {
   months: number;
-  payment?: number;
+  payment: number;
   principal: number;
   rate: number;
 }
@@ -23,5 +25,52 @@ class LoanCalculator {
   }
 
   loan: Loan;
+
+}
+
+class LoanField extends PureComponent<
+  {label: string, name: string, step?: string}
+> {
+
+  static defaultProps = {
+    step: '0.01',
+  };
+
+  change = () => {
+    console.log(this.props.name, this.input.value);
+  };
+
+  input: HTMLInputElement;
+
+  render() {
+    return <tr>
+      <td><label>{this.props.label}</label></td>
+      <td>
+        <input
+          min='0'
+          onChange={this.change}
+          ref={input => this.input = input!}
+          name={this.props.name}
+          step={this.props.step}
+          type='number'
+        />
+      </td>
+    </tr>;
+  }
+
+}
+
+class LoanForm extends PureComponent<{loan: Loan}> {
+
+  render() {
+    return <div>
+      <table>
+        <LoanField label='Months' name='months'/>
+        <LoanField label='Payment' name='payment'/>
+        <LoanField label='Principal' name='principal'/>
+        <LoanField label='Rate' name='rate' step='0.005'/>
+      </table>
+    </div>;
+  }
 
 }
